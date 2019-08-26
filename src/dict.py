35dd6b2695bb8cff9d5c443c20453c3fd4edf326
againafter10 <archana.joshi@gmail.com>
2019-08-26 19:01:37 +1000

    either json or yaml

--- src/dict.py
+++ src/dict.py
@@ -1,6 +1,7 @@
 # prototype xlsx to YAML for metadata
 import xlrd
 import yaml
+import json
 import os
 from  dict_template import *
 
@@ -32,8 +33,12 @@ for rownum in range(2, sheet.nrows):
     row_values = sheet.row_values(rownum)
     d ={}
     for i  in range(len(cols)):
-        d[cols[i]]=row_values[i]
+        if row_values[i] == "Decimal":
+            d[cols[i]]="Float"
+        else:
+            d[cols[i]]=row_values[i]
     schema.append(d)
+
 
 table_schema['target_spec']['schema']=schema
 table_schema['target_spec']['table'] = output_yaml_name
@@ -41,7 +46,9 @@ table_schema['target_spec']['dataset'] = output_yaml_name
 table_schema['target_spec']['big_queryview'] = "V_"+output_yaml_name
 table_schema['target_spec']['big_query_temp_table'] = "T_"+output_yaml_name
 
-yaml_contents=yaml.dump(table_schema)
+#yaml_contents=yaml.dump(table_schema)
+yaml_contents=json.dumps(table_schema)
 
-with open(output_yaml_name+".yaml",'w') as outfile:
+with open(output_yaml_name+".json",'w') as outfile:
+#with open(output_yaml_name+".yaml",'w') as outfile:
     outfile.write(yaml_contents)
